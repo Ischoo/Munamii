833380d69f91e38103b2cf85ea026b732bf85b74
Ischoo <isakallanson@gmail.com>
2023-08-14 16:54:56 +0200

    pathing

--- cakes.js
+++ cakes.js
@@ -1,83 +1,83 @@
 var cakes =[
     {
-    path: "/img/cake_list1.jpg",
+    path: "./img/cake_list1.jpg",
     title: "Black and White",
     price: "24.99"
     },
     {
-    path: "/img/cake_list2.jpg",
+    path: "./img/cake_list2.jpg",
     title: "Fresh Blueberries",
     price: "22.99"
     },
     {
-        path: "/img/cake_list3.jpg",
+        path: "./img/cake_list3.jpg",
         title: "Macaron Madness",
         price: "27.99"
     },
     {
-        path: "/img/cake_list4.jpg",
+        path: "./img/cake_list4.jpg",
         title: "Sweet Mango",
         price: "24.99"
     },
     {
-        path: "/img/cake_list5.jpg",
+        path: "./img/cake_list5.jpg",
         title: "Fancy Frosting",
         price: "32.99"
     },
     {
-        path: "/img/cake_list6.jpg",
+        path: "./img/cake_list6.jpg",
         title: "Summer Berries",
         price: "26.99"
     },
     {
-        path: "/img/cake_list7.jpg",
+        path: "./img/cake_list7.jpg",
         title: "Pink Oreos",
         price: "28.99"
     },
     {
-        path: "/img/cake_list8.jpg",
+        path: "./img/cake_list8.jpg",
         title: "Happy Holiday",
         price: "24.99"
     }
 ]
 var cupcakes =[
     {
-    path: "/img/cup_list1.jpg",
+    path: "./img/cup_list1.jpg",
     title: "Red Velvet",
     price: "2.99"
     },
     {
-    path: "/img/cup_list2.jpg",
+    path: "./img/cup_list2.jpg",
     title: "Sweet Lemon",
     price: "2.99"
     },
     {
-        path: "/img/cup_list3.jpg",
+        path: "./img/cup_list3.jpg",
         title: "Nutty Hazel",
         price: "2.99"
     },
     {
-        path: "/img/cup_list4.jpg",
+        path: "./img/cup_list4.jpg",
         title: "Oreo Blast",
         price: "2.99"
     },
     {
-        path: "/img/cup_list5.jpg",
+        path: "./img/cup_list5.jpg",
         title: "Cherry Top",
         price: "2.99"
     },
     {
-        path: "/img/cup_list6.jpg",
+        path: "./img/cup_list6.jpg",
         title: "Fresh Menthos",
         price: "2.99"
     },
     {
-        path: "/img/cup_list7.jpg",
+        path: "./img/cup_list7.jpg",
         title: "Rainbow Taste",
         price: "2.99"
     },
     {
-        path: "/img/cup_list8.jpg",
+        path: "./img/cup_list8.jpg",
         title: "Brownie Points",
         price: "2.99"
     }
